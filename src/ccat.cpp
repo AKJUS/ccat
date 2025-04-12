eb49dac5b7aad695c26332947fa8d628d0e5355f
Clas Onnebrink <clas@gmx.net>
2025-04-12 14:17:48 +0200

    readin arguments

--- src/ccat.cpp
+++ src/ccat.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 
 const int CF_SUCCEES = 0;
-const int CF_ERROR_OPEN = -1;
+const int CF_ERROR_INVALID_ARGS = -1;
+const int CF_ERROR_OPEN = -2;
 
 enum outFormat {
   RAW,
@@ -16,18 +17,44 @@ enum outFormat {
 };
 
 struct inFileData {
-  string filePath;
-  long startPos;
-  long preCount;
-  long postCount;
+  string path;
+  long offsetPos;
+  long offsetAround;
+  bool isComplete;
 };
+
+inFileData readFileData(int argc, char*argv[]) {
+  inFileData fileData;
+  fileData.offsetPos = 0;
+  fileData.offsetAround = 0;
+  fileData.isComplete = false;
+  for (auto i = 1; i < argc; ++i) {
+    string argument {argv[i]};
+    if (argument.substr(0,3) == "-o:")
+      fileData.offsetPos = stol(argument.substr(3, argument.length()));
+    else if (argument.substr(0,3) == "-a:")
+      fileData.offsetAround = stol(argument.substr(3, argument.length()));
+    else
+      fileData.path = argument;
+  }
+  fileData.isComplete = (fileData.offsetPos > 0 && fileData.path.length() > 0);
+  return fileData;
+}
+
+void printHelp() {
+  cout << "ccat -o:<offset> -a:<around> <file> \n";
+}
 
 int main(int argc, char *argv[])
 {
-  inFileData fileData;
-  auto cFile = fopen(fileData.filePath.c_str(), "rb");
+  inFileData fileData = readFileData(argc, argv);
+  if (fileData.isComplete == false) {
+    printHelp();
+    return CF_ERROR_INVALID_ARGS;
+  }
+  auto cFile = fopen(fileData.path.c_str(), "rb");
   if (cFile == nullptr) {
-    cout << "Cannot open file : \"" << fileData.filePath << "\" !";
+    cout << "Cannot open file : \"" << fileData.path << "\"\n";
     return CF_ERROR_OPEN;
   }
   fclose(cFile);
